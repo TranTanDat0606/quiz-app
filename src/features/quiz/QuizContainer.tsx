c5143534ca27450d571bc5d7851863fb46c54a41
Trần Đạt <trantandatpvt@gmail.com>
2025-12-30 16:51:08 +0700

    delete folder feature/quiz

--- src/features/quiz/QuizContainer.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function QuizContainer() {
-  return <div>QuizContainer</div>;
-}
-
-export default QuizContainer;
--- src/features/quiz/QuizHeader.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function QuizHeader() {
-  return <div>QuizHeader</div>;
-}
-
-export default QuizHeader;
